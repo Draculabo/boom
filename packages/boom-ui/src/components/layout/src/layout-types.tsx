7e8880c30c64e5e92ffe7d329dfaeaeedde084d0
Norn3 <1430835578@qq.com>
2023-01-25 21:55:15 +0800

    test(pnpm test): test for pnpm cz

--- packages/boom-ui/src/components/layout/src/layout-types.tsx
+++ packages/boom-ui/src/components/layout/src/layout-types.tsx
@@ -1,4 +1,2 @@
 //无侧边栏、左侧边栏、右侧边栏，两侧侧边栏、无页脚、无页头
 export type LayoutType = 'no-aside' | 'left-aside' | 'right-aside' | 'both-aside' | 'no-footer' | 'no-header';
-//普通页头导航栏、外部投影导航栏、内部投影导航栏
-export type HeaderType = 'normal' | 'outside-shadow' | 'inside-shadow';
