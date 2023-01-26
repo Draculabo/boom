46954118842e568ab0a318ee602b945779548196
degulabo <1842298680@qq.com>
2023-01-26 12:24:58 +0800

    chore(project): optimize lintstaged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'packages/**/{*.vue,*.js,*.ts,*.jsx,*.tsx}': ['eslint --fix', 'prettier --write'],
-  'packages/**/{*.scss,*.css}': 'stylelint --fix',
+  'packages/**/{*.vue,*.js,*.ts,*.jsx,*.tsx}': ['eslint --fix --max-warnings=0 --cache', 'prettier --write --ignore-unknown ${staged}'],
+  'packages/**/{*.scss,*.css}': 'stylelint --fix --cache',
 };
