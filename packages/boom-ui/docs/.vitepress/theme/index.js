8368196e6665fc876e26045b02391b4d04160a13
Jacqueese <66241844+wangyu13142@users.noreply.github.com>
2023-02-12 19:33:15 +0800

    fill relevant buttn document

--- packages/boom-ui/docs/.vitepress/theme/index.js
+++ packages/boom-ui/docs/.vitepress/theme/index.js
@@ -2,11 +2,13 @@ import DefaultTheme from 'vitepress/theme';
 import 'vitepress-theme-demoblock/dist/theme/styles/index.css';
 import { useComponents } from './useComponents';
 import Status from '../../../src/components/status';
+import BoomButton from '../../../src/components/button';
 export default {
   ...DefaultTheme,
   enhanceApp(ctx) {
     DefaultTheme.enhanceApp(ctx);
     useComponents(ctx.app);
     ctx.app.component(Status.name, Status);
+    ctx.app.component(BoomButton.name, BoomButton);
   },
 };
